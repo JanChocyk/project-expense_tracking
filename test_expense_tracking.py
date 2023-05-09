b14586fdd0f4f2ae59a18760e37b274ab954778f
JanChocyk <janischocyk@gmail.com>
2023-05-09 20:51:38 +0200

    Update tests.

--- test_expense_tracking.py
+++ test_expense_tracking.py
@@ -1,39 +1,3 @@
-# MySQL
-# python expense_tracking.py add mysql 1001 "test"
-# python expense_tracking.py raport mysql
-# python expense_tracking.py import-csv mysql "expenses.csv" 
-# python expense_tracking.py python-export mysql
-# python expense_tracking.py delete mysql 1
-# python expense_tracking.py drop-database mysql
-
-# SQLite
-# python expense_tracking.py add sqlite 1001 "test"
-# python expense_tracking.py raport sqlite
-# python expense_tracking.py import-csv sqlite "expenses.csv" 
-# python expense_tracking.py python-export sqlite
-# python expense_tracking.py delete sqlite 1
-# python expense_tracking.py drop-database sqlite
-
-# SCENARIUSZ TESTÓW:
-# 
-# 1. test init_db_connection(choice_db: str)
-# Sprawdzenie czy działa if, który decyduje, czy dostejemy obiekt db= MySQLConnection() czy db=SQLConnection()
-# 
-# 2. test read_db(Connector)
-# Sprawdzenie czy działa SELECT i czy jest zwracana lista obiektów Expense.
-# 
-# 3. test metody save_to_db(db, amoutn, description)
-# Sprawdzenie, czy poprawnie zadziałał INSERT
-# 
-# 4. Test inita klasy Expense
-# - utworzenie poprawnego obiektu
-# - sprawdzenie czy jest rzucany ValueError
-# 
-# 5. Test metody execute_on_cursor(query, val)
-# -select
-# -insert
-# -delete
-
 import csv
 
 import pytest
